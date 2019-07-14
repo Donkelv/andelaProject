47311d52037827cbc85ebe2ecf3670e671ae6257
Donkelv <kevinohiro@gmail.com>
2019-07-14 23:19:14 +0100

    project Andela first commit

---
+++ app/src/main/java/com/example/projectandela/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.projectandela;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private Button button4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityabout();
+            }
+        });
+
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityprofile();
+            }
+        });
+    }
+    public void openActivityabout(){
+        Intent intent = new Intent(this, aboutAndela.class);
+        startActivity(intent);
+    }
+    public void openActivityprofile(){
+        Intent intent = new Intent(this, myProfile.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/projectandela/aboutAndela.java
@@ -0,0 +1,28 @@
+package com.example.projectandela;
+
+import android.net.http.SslError;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class aboutAndela extends AppCompatActivity {
+    String url = "https://andela.com/alc/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_andela);
+        WebView webView = findViewById(R.id.alcWebView);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+
+    }
+}
